15d77b9160ca154f1c462898cef5e4fa747540bd
Chris Keefe <crk239@nau.edu>
2020-12-04 15:18:22 -0700

    Swaps cinnamonConfig for gnomeConfig

---
+++ gnomeConfig.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+set -ex
+# TODO: confirm
+# default mouse-button-modifier keybinding clobbers vscode multi-cursor, so...
+# gsettings set org.cinnamon.desktop.wm.preferences mouse-button-modifier "<Super>"
+
+# TODO: set DE sounds as needed
+
+# TODO: Set "Home" key to sleep
