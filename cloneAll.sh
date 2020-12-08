0eb3d969191eb0164a2d1890c9e744c918a9f1aa
Chris Keefe <crk239@nau.edu>
2020-12-08 13:30:50 -0700

    Removes set -e from cloneAll to prevent premature halt

--- cloneAll.sh
+++ cloneAll.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
-set -e
-
-# TODO: segregate q2 repos into separate subdir
+# This script is somewhat fragile, so no set -e
+# With set -e, it halts on l17, presumably when jq returns a nonzero exit code
 read -p "What is your GH username? " UNAME
 ORG="qiime2"
 export ORG
@@ -17,12 +16,15 @@ fi
 
 cd $INSTALL_DIR
 USER_JSON=$(curl -s "https://${GHTKN}:@api.github.com/users/${UNAME}/repos?per_page=200")
-SSH_URLS=$(echo $USER_JSON | jq .[].ssh_url)
+SSH_URLS=$(echo $USER_JSON | jq .[].ssh_url || true; echo "Success" )
 echo $SSH_URLS | xargs -n 1 git clone
+
+printf "\nClone completed\n"
 
 ORG_JSON=$(curl -s "https://${GHTKN}:@api.github.com/orgs/${ORG}/repos?per_page=200")
 ORG_REPO_NAMES=$(echo $ORG_JSON | jq .[].name)
 
+echo $ORG_REPO_NAMES
 # Takes a list of repo names from an org ( or maybe a user ) and segregates them into an org folder
 # Must be called from within the directory containing listed repositories
 move_repos () {
@@ -45,13 +47,16 @@ add_upstream_remote () {
 }
 export -f add_upstream_remote
 
+printf "\nFunctions defined/exported\n"
 # NOTE: This currently segregates and adds upstream remotes ONLY for $ORG repos (qiime2 in this case).
-# TODO: Refactor to add upstream remotes for all user repos (where upstream repos exist)
+# TODO: Add upstream remotes for all user repos (where upstream repos exist)
 # Consider API calls to individual repos
 # (e.g. https://stackoverflow.com/questions/18580913/github-api-for-a-forked-repository-object-how-to-get-what-repository-its-fork)
 cd $INSTALL_DIR
 echo $ORG_REPO_NAMES | xargs -n 1 bash -c 'move_repos "$@"' _
+printf "\nRepos moved\n"
 echo $ORG_REPO_NAMES | xargs -n 1 bash -c 'add_upstream_remote "$@"' _
+printf "\nRemotes added\n"
 
 # Rename aliased org_dir to original org name
 mv ${ORGDIR} $HOME/src/${ORG}
